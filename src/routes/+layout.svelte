b4617b12b2d498a9cea47398c3589c9b88614e99
radoslavsapundzhiev <radoslavsapundzhiev@gmail.com>
2024-04-19 18:01:54 +0300

    added search and addtodoform components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,42 +1,50 @@
 <section class="hero has-background-grey block">
   <div class="hero-body">
-    <p class="title has-text-centered">Link hero</p>
-    <p class="subtitle has-text-centered">Link subtitle</p>
+    <p class="title has-text-centered">Todo App</p>
+    <p class="subtitle has-text-centered">Todo App</p>
   </div>
 </section>
 
-<main class="block">
-  <div class="columns">
-    <div class="column is-2 has-background-grey">
-      <aside class="menu">
-        <p class="menu-label has-text-light">Todo menu</p>
-        <ul class="menu-list">
-          <li class="is-hoverable">
-            <a class="has-background-grey has-text-light"
-              >Mark all as resolved</a
-            >
-          </li>
-          <li>
-            <a class="has-background-grey has-text-light"
-              >Mark all as unresolved</a
-            >
-          </li>
-          <li>
-            <a class="has-background-grey has-text-light">Remove all todos</a>
-          </li>
-          <li><a class="has-background-grey has-text-light">Add Todo</a></li>
-        </ul>
-      </aside>
-    </div>
+<div class="columns is-flex-direction-column is-fullheight-100vh">
+  <main class="column">
+    <div class="columns">
+      <div class="column is-2 has-background-grey">
+        <aside class="menu">
+          <p class="menu-label has-text-light">Todo menu</p>
+          <ul class="menu-list">
+            <li class="is-hoverable">
+              <a class="has-background-grey has-text-light"
+                >Mark all as resolved</a
+              >
+            </li>
+            <li>
+              <a class="has-background-grey has-text-light"
+                >Mark all as unresolved</a
+              >
+            </li>
+            <li>
+              <a class="has-background-grey has-text-light">Remove all todos</a>
+            </li>
+            <li><a class="has-background-grey has-text-light">Add Todo</a></li>
+          </ul>
+        </aside>
+      </div>
 
-    <div class="column is-10 has-background-grey-light">
-      <slot />
+      <div class="column is-10 has-text-light">
+        <slot />
+      </div>
     </div>
-  </div>
-</main>
+  </main>
 
-<footer class="footer has-background-grey has-text-light">
-  <div class="content has-text-centered">
-    <small> Copyright © 2024 Todo App. All Rights Reserved. </small>
-  </div>
-</footer>
+  <footer class="column has-background-grey has-text-light is-narrow">
+    <div class="content has-text-centered">
+      <small> Copyright © 2024 Todo App. All Rights Reserved. </small>
+    </div>
+  </footer>
+</div>
+
+<style>
+  .is-fullheight-100vh {
+    height: 100vh;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,9 @@
+<script>
+  import Search from "../components/Search.svelte";
+  import AddTodoForm from "../components/AddTodoForm.svelte";
+</script>
+
 <h1 class="title">Home</h1>
+
+<Search />
+<AddTodoForm />
